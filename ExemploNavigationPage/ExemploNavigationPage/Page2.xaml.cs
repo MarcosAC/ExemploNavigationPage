5fa7ce6b453708e404d6ba221a1eb26c199df033
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-01-31 14:35:27 -0300

    Exemplo NavigationPage

---
+++ ExemploNavigationPage/ExemploNavigationPage/Page2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExemploNavigationPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
+                
+        private void IrPagina3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ ExemploNavigationPage/ExemploNavigationPage/Page3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExemploNavigationPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : ContentPage
+	{
+		public Page3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void IrVoltarPagina1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
